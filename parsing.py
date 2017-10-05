fbe9360a23b6f2f38fdfb981bc65992f24a3611d
Mihika0409 <mihika.shah@asu.edu>
2017-10-04 19:42:33 -0700

    Added comments to parsing.py

--- parsing.py
+++ parsing.py
@@ -5,6 +5,7 @@ import unicodecsv
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+#Sample to run stop words.
 '''data = "All work and no play makes jack a dull boy."
 stopWords = set(stopwords.words('english'))
 words = word_tokenize(data)
@@ -14,8 +15,9 @@ for w in words:
     if w not in stopWords:
         wordsFiltered.append(w)
  
-print(wordsFiltered)
+print(wordsFiltered)'''
 
+#Converts the xls file to csv
 def xls2csv (xls_filename, csv_filename):
 
     wb = open_workbook(xls_filename)
@@ -25,14 +27,14 @@ def xls2csv (xls_filename, csv_filename):
     for row_number in xrange (sh.nrows):
         csv_out.writerow(sh.row_values(row_number))
     fh.close()
-    return csv_filename'''
+    return csv_filename
 
-#csv_file = xls2csv('Copy of Trauma Data Sample From Jan 2016 to Jan 2017.xlsx','csv.csv')
+csv_file = xls2csv('Copy of Trauma Data Sample From Jan 2016 to Jan 2017.xlsx','csv.csv')
 
+#creates a new dataframe with just comments
 df = pd.read_csv('csv.csv')
 df1 = df.ix[:, 19:20].dropna(axis=0, how='any')
 df1 = df1.rename(columns = {'Unnamed: 19':'Comments'})
-#print df1
 
 for index, row in df1.iterrows():
     print row['Comments']
