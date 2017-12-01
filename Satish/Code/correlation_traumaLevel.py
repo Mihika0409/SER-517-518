4b8fb4d578f4503a6d16d1c39794faaaeda276ec
Satish Nandan Reddy Lakkam <satish.lakkam@gmail.com>
2017-12-01 11:34:03 -0700

    Updated mv speed with GCS, RR, PULSE, SBP

--- Satish/Code/correlation_traumaLevel.py
+++ Satish/Code/correlation_traumaLevel.py
@@ -17,7 +17,7 @@ print cor_matrix
 print cor_matrix1
 
 #To find the pearson and spearmanr corelation between The SBP and the trauma level.
-df = pd.read_csv('sbp.csv')
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/sbp.csv')
 cols = ['SBP','Levels']
 cols1 = ['SBP']
 cols2 = ['Levels']
@@ -28,7 +28,7 @@ print cor_matrix
 print cor_matrix1
 
 #To find the pearson and spearmanr corelation between The GCS and the trauma level.
-df = pd.read_csv('gcs.csv')
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/gcs.csv')
 cols = ['GCS','Levels']
 cols1 = ['GCS']
 cols2 = ['Levels']
@@ -39,7 +39,7 @@ print cor_matrix
 print cor_matrix1
 
 #To find the pearson and spearmanr corelation between The Respiratory Rate and the trauma level.
-df = pd.read_csv('rr.csv')
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/rr.csv')
 cols = ['RR','Levels']
 cols1 = ['RR']
 cols2 = ['Levels']
@@ -50,7 +50,7 @@ print cor_matrix
 print cor_matrix1
 
 #To find the pearson and spearmanr corelation between The Respiratory Rate and the trauma level.
-df = pd.read_csv('Pulse.csv')
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/Pulse.csv')
 cols = ['Pulse','Levels']
 cols1 = ['Pulse']
 cols2 = ['Levels']
@@ -60,8 +60,53 @@ print "Pulse and Trauma Level"
 print cor_matrix
 print cor_matrix1
 
+#To find the pearson and spearmanr corelation between The motor vehicle speed and the GCS.
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/GCSmv.csv')
+cols = ['MV Speed','GCS']
+cols1 = ['MV Speed']
+cols2 = ['GCS']
+cor_matrix1 = np.corrcoef(df[cols].values.T)
+cor_matrix = sp.spearmanr(df[cols1].values,df[cols2].values)
+print "Mv Speed and GCS"
+print cor_matrix
+print cor_matrix1
+
+#To find the pearson and spearmanr corelation between The motor vehicle speed and the RR.
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/GCSrr.csv')
+cols = ['MV Speed','RR']
+cols1 = ['MV Speed']
+cols2 = ['RR']
+cor_matrix1 = np.corrcoef(df[cols].values.T)
+cor_matrix = sp.spearmanr(df[cols1].values,df[cols2].values)
+print "Mv Speed and RR"
+print cor_matrix
+print cor_matrix1
+
+#To find the pearson and spearmanr corelation between The motor vehicle speed and the Pulse.
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/GCSpulse.csv')
+cols = ['MV Speed','Pulse']
+cols1 = ['MV Speed']
+cols2 = ['Pulse']
+cor_matrix1 = np.corrcoef(df[cols].values.T)
+cor_matrix = sp.spearmanr(df[cols1].values,df[cols2].values)
+print "Mv Speed and Pulse"
+print cor_matrix
+print cor_matrix1
+
+#To find the pearson and spearmanr corelation between The motor vehicle speed and the SBP.
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/GCSsbp.csv')
+cols = ['MV Speed','SBP']
+cols1 = ['MV Speed']
+cols2 = ['SBP']
+cor_matrix1 = np.corrcoef(df[cols].values.T)
+cor_matrix = sp.spearmanr(df[cols1].values,df[cols2].values)
+print "Mv Speed and SBP"
+print cor_matrix
+print cor_matrix1
+
+
 #will remove not available, no data and blank entries from data set.
-with open('test.csv', 'rb') as inp, open('Pulse.csv', 'wb') as out:
+with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/Pulse.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[23].strip() == "*NA" or row[23].strip() == "*ND" or row[23].strip() == "*BL":
@@ -71,7 +116,7 @@ with open('test.csv', 'rb') as inp, open('Pulse.csv', 'wb') as out:
 
 
 #will remove not available, no data and blank entries from data set.
-with open('test.csv', 'rb') as inp, open('mv2.csv', 'wb') as out:
+with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/mv2.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[18].strip() == "*NA" or row[18].strip() == "*ND" or row[18].strip() == "*BL":
@@ -80,7 +125,7 @@ with open('test.csv', 'rb') as inp, open('mv2.csv', 'wb') as out:
             writer.writerow(row)
 
 #will remove not available, no data and blank entries from data set.
-with open('test.csv', 'rb') as inp, open('sbp.csv', 'wb') as out:
+with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/sbp.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[22].strip() == "*NA" or row[22].strip() == "*ND" or row[22].strip() == "*BL":
@@ -89,7 +134,7 @@ with open('test.csv', 'rb') as inp, open('sbp.csv', 'wb') as out:
             writer.writerow(row)
 
 #will remove not available, no data and blank entries from data set.
-with open('test.csv', 'rb') as inp, open('gcs.csv', 'wb') as out:
+with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/gcs.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[25].strip() == "*NA" or row[25].strip() == "*ND" or row[25].strip() == "*BL":
@@ -98,10 +143,46 @@ with open('test.csv', 'rb') as inp, open('gcs.csv', 'wb') as out:
             writer.writerow(row)
 
 #will remove not available, no data and blank entries from data set.
-with open('test.csv', 'rb') as inp, open('rr.csv', 'wb') as out:
+with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/rr.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[24].strip() == "*NA" or row[24].strip() == "*ND" or row[24].strip() == "*BL":
+            continue
+        else:
+            writer.writerow(row)
+
+#will remove not available, no data and blank entries from data set.
+with open('/Users/satishnandan/Desktop/TraumaActivation/mv2.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/GCSmv.csv', 'wb') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if row[25].strip() == "*NA" or row[25].strip() == "*ND" or row[25].strip() == "*BL":
+            continue
+        else:
+            writer.writerow(row)
+
+#will remove not available, no data and blank entries from data set.
+with open('/Users/satishnandan/Desktop/TraumaActivation/mv2.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/GCSrr.csv', 'wb') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if row[24].strip() == "*NA" or row[24].strip() == "*ND" or row[24].strip() == "*BL":
+            continue
+        else:
+            writer.writerow(row)
+
+#will remove not available, no data and blank entries from data set.
+with open('/Users/satishnandan/Desktop/TraumaActivation/mv2.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/GCSPulse.csv', 'wb') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if row[23].strip() == "*NA" or row[23].strip() == "*ND" or row[23].strip() == "*BL":
+            continue
+        else:
+            writer.writerow(row)
+
+#will remove not available, no data and blank entries from data set.
+with open('/Users/satishnandan/Desktop/TraumaActivation/mv2.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/GCSsbp.csv', 'wb') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if row[22].strip() == "*NA" or row[22].strip() == "*ND" or row[22].strip() == "*BL":
             continue
         else:
             writer.writerow(row)
