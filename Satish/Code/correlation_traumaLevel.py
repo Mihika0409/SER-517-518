8dc798b3057a0508b0ac0744e28d4244b2f88f41
Satish Nandan Reddy Lakkam <satish.lakkam@gmail.com>
2017-12-01 11:07:39 -0700

    Updated With spearmanr

--- Satish/Code/correlation_traumaLevel.py
+++ Satish/Code/correlation_traumaLevel.py
@@ -1,37 +1,64 @@
-import csv
-
-import numpy as np
 import pandas as pd
+import numpy as np
 
-#To find the pearson corelation between The motor vehicle speed and the trauma level.
-df = pd.read_csv('mv2.csv')
+import scipy.stats as sp
+import csv
+import matplotlib.pyplot as plt
+
+#To find the pearson and spearmanr corelation between The motor vehicle speed and the trauma level.
+df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/mv2.csv')
 cols = ['MV Speed','Levels']
-cor_matrix = np.corrcoef(df[cols].values.T)
+cols1 = ['MV Speed']
+cols2 = ['Levels']
+cor_matrix1 = np.corrcoef(df[cols].values.T)
+cor_matrix = sp.spearmanr(df[cols1].values,df[cols2].values)
+print "Mv Speed and Trauma Level"
 print cor_matrix
+print cor_matrix1
 
-#To find the pearson corelation between The SBP and the trauma level.
+#To find the pearson and spearmanr corelation between The SBP and the trauma level.
 df = pd.read_csv('sbp.csv')
 cols = ['SBP','Levels']
-cor_matrix = np.corrcoef(df[cols].values.T)
+cols1 = ['SBP']
+cols2 = ['Levels']
+cor_matrix = sp.spearmanr(df[cols1].values,df[cols2].values)
+cor_matrix1 = np.corrcoef(df[cols].values.T)
+print "SBP and Trauma Level"
 print cor_matrix
+print cor_matrix1
 
-#To find the pearson corelation between The GCS and the trauma level.
+#To find the pearson and spearmanr corelation between The GCS and the trauma level.
 df = pd.read_csv('gcs.csv')
 cols = ['GCS','Levels']
+cols1 = ['GCS']
+cols2 = ['Levels']
+cor_matrix1 = sp.spearmanr(df[cols1].values,df[cols2].values)
 cor_matrix = np.corrcoef(df[cols].values.T)
+print "GCS and Trauma Level"
 print cor_matrix
+print cor_matrix1
 
-#To find the pearson corelation between The Respiratory Rate and the trauma level.
+#To find the pearson and spearmanr corelation between The Respiratory Rate and the trauma level.
 df = pd.read_csv('rr.csv')
 cols = ['RR','Levels']
+cols1 = ['RR']
+cols2 = ['Levels']
+cor_matrix1 = sp.spearmanr(df[cols1].values,df[cols2].values)
 cor_matrix = np.corrcoef(df[cols].values.T)
+print "RR and Trauma Level"
 print cor_matrix
+print cor_matrix1
 
-#To find the pearson corelation between The Pulse  and the trauma level.
+#To find the pearson and spearmanr corelation between The Respiratory Rate and the trauma level.
 df = pd.read_csv('Pulse.csv')
 cols = ['Pulse','Levels']
+cols1 = ['Pulse']
+cols2 = ['Levels']
+cor_matrix1 = sp.spearmanr(df[cols1].values,df[cols2].values)
 cor_matrix = np.corrcoef(df[cols].values.T)
+print "Pulse and Trauma Level"
 print cor_matrix
+print cor_matrix1
 
 #will remove not available, no data and blank entries from data set.
 with open('test.csv', 'rb') as inp, open('Pulse.csv', 'wb') as out:
