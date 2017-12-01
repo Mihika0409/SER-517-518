7914ec3c8a0ac015d9629984425239d9a3d0dfc1
Satish Nandan Reddy Lakkam <satish.lakkam@gmail.com>
2017-11-30 19:57:30 -0700

    Changed File Path

--- Satish/Code/correlation_traumaLevel.py
+++ Satish/Code/correlation_traumaLevel.py
@@ -4,37 +4,37 @@ import numpy as np
 import pandas as pd
 
 #To find the pearson corelation between The motor vehicle speed and the trauma level.
-df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/mv2.csv')
+df = pd.read_csv('mv2.csv')
 cols = ['MV Speed','Levels']
 cor_matrix = np.corrcoef(df[cols].values.T)
 print cor_matrix
 
 #To find the pearson corelation between The SBP and the trauma level.
-df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/sbp.csv')
+df = pd.read_csv('sbp.csv')
 cols = ['SBP','Levels']
 cor_matrix = np.corrcoef(df[cols].values.T)
 print cor_matrix
 
 #To find the pearson corelation between The GCS and the trauma level.
-df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/gcs.csv')
+df = pd.read_csv('gcs.csv')
 cols = ['GCS','Levels']
 cor_matrix = np.corrcoef(df[cols].values.T)
 print cor_matrix
 
 #To find the pearson corelation between The Respiratory Rate and the trauma level.
-df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/rr.csv')
+df = pd.read_csv('rr.csv')
 cols = ['RR','Levels']
 cor_matrix = np.corrcoef(df[cols].values.T)
 print cor_matrix
 
 #To find the pearson corelation between The Pulse  and the trauma level.
-df = pd.read_csv('/Users/satishnandan/Desktop/TraumaActivation/Pulse.csv')
+df = pd.read_csv('Pulse.csv')
 cols = ['Pulse','Levels']
 cor_matrix = np.corrcoef(df[cols].values.T)
 print cor_matrix
 
 #will remove not available, no data and blank entries from data set.
-with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/Pulse.csv', 'wb') as out:
+with open('test.csv', 'rb') as inp, open('Pulse.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[23].strip() == "*NA" or row[23].strip() == "*ND" or row[23].strip() == "*BL":
@@ -44,7 +44,7 @@ with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp,
 
 
 #will remove not available, no data and blank entries from data set.
-with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/mv2.csv', 'wb') as out:
+with open('test.csv', 'rb') as inp, open('mv2.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[18].strip() == "*NA" or row[18].strip() == "*ND" or row[18].strip() == "*BL":
@@ -53,7 +53,7 @@ with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp,
             writer.writerow(row)
 
 #will remove not available, no data and blank entries from data set.
-with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/sbp.csv', 'wb') as out:
+with open('test.csv', 'rb') as inp, open('sbp.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[22].strip() == "*NA" or row[22].strip() == "*ND" or row[22].strip() == "*BL":
@@ -62,7 +62,7 @@ with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp,
             writer.writerow(row)
 
 #will remove not available, no data and blank entries from data set.
-with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/gcs.csv', 'wb') as out:
+with open('test.csv', 'rb') as inp, open('gcs.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[25].strip() == "*NA" or row[25].strip() == "*ND" or row[25].strip() == "*BL":
@@ -71,7 +71,7 @@ with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp,
             writer.writerow(row)
 
 #will remove not available, no data and blank entries from data set.
-with open('/Users/satishnandan/Desktop/TraumaActivation/test.csv', 'rb') as inp, open('/Users/satishnandan/Desktop/TraumaActivation/rr.csv', 'wb') as out:
+with open('test.csv', 'rb') as inp, open('rr.csv', 'wb') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
         if row[24].strip() == "*NA" or row[24].strip() == "*ND" or row[24].strip() == "*BL":
