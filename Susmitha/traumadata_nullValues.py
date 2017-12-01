6b111cc29238724fd935ba3678e4f306a16f8070
susmitha8 <skistams@asu.edu>
2017-11-30 21:22:42 -0700

    Calculating the percentage of null values

--- Susmitha/traumadata_nullValues.py
+++ Susmitha/traumadata_nullValues.py
@@ -24,6 +24,10 @@ for i in range(0, len(fall_height)):
     elif fall_height[i] == "*BL":
         bl_count_fallheight = bl_count_fallheight + 1
 print (na_count_fallheight)
+total = na_count_fallheight + nd_count_fallheight + bl_count_fallheight
+print ('--------------------------')
+fall_height_nullvalues_percent = (total/float(len(fall_height)))*100
+print (fall_height_nullvalues_percent)
 
 trauma_type = df['trauma_type']
 print(trauma_type)
@@ -40,6 +44,10 @@ for i in range(0, len(trauma_type)):
     elif trauma_type[i] == "*BL":
         bl_count_traumatype = bl_count_traumatype + 1
 print (nd_count_traumatype)
+total = na_count_traumatype + nd_count_traumatype + bl_count_traumatype
+print ('--------------------------')
+trauma_type_nullvalues_percent = (total/float(len(trauma_type)))*100
+print (trauma_type_nullvalues_percent)
 
 airbag_deploy = df['airbag_deploy']
 print (airbag_deploy)
@@ -121,7 +129,7 @@ for i in range(0, len(mv_speed)):
         bl_count_mv_speed = bl_count_mv_speed + 1
 print (na_count_mv_speed)
 
-'''transport_mode = df['transport_mode']
+transport_mode = df['transport_mode']
 print (transport_mode)
 na_count_transport_mode = 0
 nd_count_transport_mode = 0
@@ -135,4 +143,100 @@ for i in range(0, len(transport_mode)):
 
     elif transport_mode[i] == "*BL":
         bl_count_transport_mode = bl_count_transport_mode + 1
-print (bl_count_transport_mode)'''
+print (bl_count_transport_mode)
+
+field_SBP1 = df['field_SBP']
+print (field_SBP1)
+na_count_field_SBP = 0
+nd_count_field_SBP = 0
+bl_count_field_SBP = 0
+for i in range(0, len(field_SBP1)):
+    if field_SBP1[i] == "*NA":
+        na_count_field_SBP = na_count_field_SBP + 1
+
+    elif field_SBP1[i] == "*ND":
+        nd_count_field_SBP = nd_count_field_SBP + 1
+
+    elif field_SBP1[i] == "*BL":
+        bl_count_field_SBP = bl_count_field_SBP + 1
+print (na_count_field_SBP)
+
+total_vent_days = df['total_vent_days']
+print (total_vent_days)
+na_count_total_vent_days = 0
+nd_count_total_vent_days = 0
+bl_count_total_vent_days = 0
+for i in range(0, len(total_vent_days)):
+    if total_vent_days[i] == "*NA":
+        na_count_total_vent_days = na_count_total_vent_days + 1
+
+    elif total_vent_days[i] == "*ND":
+        nd_count_total_vent_days = nd_count_total_vent_days + 1
+
+    elif total_vent_days[i] == "*BL":
+        bl_count_total_vent_days = bl_count_total_vent_days + 1
+print (na_count_total_vent_days)
+
+field_HR = df['field_HR']
+print (field_HR)
+na_count_field_HR = 0
+nd_count_field_HR = 0
+bl_count_field_HR = 0
+for i in range(0, len(field_HR)):
+    if field_HR[i] == "*NA":
+        na_count_field_HR = na_count_field_HR + 1
+
+    elif field_HR[i] == "*ND":
+        nd_count_field_HR = nd_count_field_HR + 1
+
+    elif field_HR[i] == "*BL":
+        bl_count_field_HR = bl_count_field_HR + 1
+print (na_count_field_HR)
+
+field_shock_ind = df['field_shock_ind']
+print (field_shock_ind)
+na_count_field_shock_ind = 0
+nd_count_field_shock_ind = 0
+bl_count_field_shock_ind = 0
+for i in range(0, len(field_shock_ind)):
+    if field_shock_ind[i] == "*NA":
+        na_count_field_shock_ind = na_count_field_shock_ind + 1
+
+    elif field_shock_ind[i] == "*ND":
+        nd_count_field_shock_ind = nd_count_field_shock_ind + 1
+
+    elif field_shock_ind[i] == "*BL":
+        bl_count_field_shock_ind = bl_count_field_shock_ind + 1
+print (na_count_field_shock_ind)
+
+ED_GCS = df['ED_GCS']
+print (ED_GCS)
+na_count_ED_GCS = 0
+nd_count_ED_GCS = 0
+bl_count_ED_GCS = 0
+for i in range(0, len(ED_GCS)):
+    if ED_GCS[i] == "*NA":
+        na_count_ED_GCS = na_count_ED_GCS + 1
+
+    elif ED_GCS[i] == "*ND":
+        nd_count_ED_GCS = nd_count_ED_GCS + 1
+
+    elif ED_GCS[i] == "*BL":
+        bl_count_ED_GCS = bl_count_ED_GCS + 1
+print (na_count_ED_GCS)
+
+brain_injury = df['brain_injury']
+print (brain_injury)
+na_count_brain_injury = 0
+nd_count_brain_injury = 0
+bl_count_brain_injury = 0
+for i in range(0, len(brain_injury)):
+    if brain_injury[i] == "*NA":
+        na_count_brain_injury = na_count_brain_injury + 1
+
+    elif brain_injury[i] == "*ND":
+        nd_count_brain_injury = nd_count_brain_injury + 1
+
+    elif brain_injury[i] == "*BL":
+        bl_count_brain_injury = bl_count_brain_injury + 1
+print (na_count_brain_injury)
