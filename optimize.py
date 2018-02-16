3cf9f4b8f269708711154a0ecee5550c8a0646b0
Satish Nandan Reddy Lakkam <satish.lakkam@gmail.com>
2018-02-15 17:45:39 -0700

    #32 Included Decision Tree Classifier with parameters tuned
    Progress So Far:
    
    Logistic Regression:
    
    *Improved Logistic Regression model by making sure model dose not over fit.
    *Used class weight attribute and made the classes balanced so as to ensure logistic regression performs accurately.
    *Used Laso (L1) as the regularization Norm and the regularization parameter(C or 1/Lambda) is kept at default 1.
    *Plotted the ROC curve and Improved the area under ROC curve from 0.5 to 0.68.
    *Came to a conclusion that PCA is not helping in the case of Logistic Regression and the model is performing poorly and the performance of the model is good with Original data.
    
    Decision Trees:
    
    *Ensured that the model does not over fit by comparing the  training and test accuracy and even used kfold cross validation to validate the results.
    *Tried different criterion for decision tree. default was gini computing gini impurity and tried using entropy for information gain. This didn't improve the accuracy of the model.
    *Changed the random state to 42 so as to ensure that model trains on ideal random seed.
    *Tried to reduce the depth of decision trees so as to see if by letting maximum features participate in giving the decision will improve the accuracy of the model. This didn't help in improving the accuracy of the model.
    *Used balanced class weight to ensure the models learns on equal proportion of output classifiers.
    *PCA is not helping in improving the accuracy and the model is performing better without PCA.

--- optimize.py
+++ optimize.py
@@ -5,18 +5,21 @@ import statsmodels.api as sm
 from sklearn.linear_model import LogisticRegression
 from sklearn import model_selection
 from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
 fields = ['Age in Years', 'Gender', 'MV Speed','Resp Assistance','RTS','Field GCS','Field SBP', 'Field HR', 'Field Shock Index', 'Field RR']
 df = pd.read_csv(
-    filepath_or_buffer='/Users/satishnandan/Desktop/TraumaActivation/mvSpeedTrauma5.csv',
+    filepath_or_buffer='mvSpeedTrauma5.csv',
     usecols=fields)
 fields1 = ['Levels']
 Y = pd.read_csv(
-    filepath_or_buffer='/Users/satishnandan/Desktop/TraumaActivation/mvSpeedTrauma5.csv',
+    filepath_or_buffer='mvSpeedTrauma5.csv',
     usecols=fields1)
 
+#Function to train the logitic regression model and compute the accuracy on the test set and also cross vaildate using k fold.
 def logistic(X,Y):
     logit_model = sm.Logit(Y, X)
     result = logit_model.fit()
@@ -26,6 +29,7 @@ def logistic(X,Y):
     logreg.fit(X_train, y_train)
     y_pred = logreg.predict(X_test)
     print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+    print('Accuracy of logistic regression classifier on training set: {:.2f}'.format(logreg.score(X_train, y_train)))
     kfold = model_selection.KFold(n_splits=10, random_state=42)
     modelCV = LogisticRegression(penalty='l1',class_weight='balanced')
     scoring = 'accuracy'
@@ -47,7 +51,23 @@ def logistic(X,Y):
     plt.savefig('Log_ROC')
     plt.show()
 
-
+#Function to train the Decision Trees Classifier and compute the accuracy on the test set and also cross vaildate using k fold.
+def decisionClassifier(X,Y):
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=100)
+    clf = DecisionTreeClassifier(criterion="gini",presort=True, random_state=42,class_weight='balanced',splitter='best',max_depth=10,min_samples_leaf=1)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    yTrain_pred = clf.predict(X_train)
+    print "Test Accuracy is ", accuracy_score(y_test, y_pred)
+    print "Training Accuracy is ", accuracy_score(y_train, yTrain_pred)
+    kfold = model_selection.KFold(n_splits=10, random_state=42)
+    modelCV = DecisionTreeClassifier(criterion="gini",presort=True,random_state=42,class_weight='balanced',splitter='best',max_depth=10,min_samples_leaf=1)
+    scoring = 'accuracy'
+    results = model_selection.cross_val_score(modelCV, X_train, y_train, cv=kfold, scoring=scoring)
+    print("10-fold cross validation average accuracy: %.3f" % (results.mean()))
 
 print "Efficiency and ROC Graph on Logistic Regression Model:"
 logistic(df,Y)
+
+print "Efficiency on Decision Tree Classifier Model:"
+decisionClassifier(df,Y)
