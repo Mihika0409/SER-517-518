6c02454636a85a41f5f1541a952d5ba24463dcf7
Vibhuti Tripathi <vtripat3@asu.edu>
2018-03-01 18:07:01 -0700

    Task #46 Implemented Initial Factor Analysis

---
+++ Factor Analysis.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.decomposition import FactorAnalysis
+
+import pandas as pd
+
+df = pd.read_csv('newoutputfile.csv', header = None, error_bad_lines=False)
+
+df.columns =['PatientID','T1','ED/Hosp Arrival Dategg',	'Age in Yearss',	'Levels',	'ICD-10 E-code',	'Injury Comments',	'Child Restraint'	,'Transport Type',	'Field SBP',	'Field HR',	'Field Shock Index',	'Field RR',	'RTS',	'ED LOS (mins)',	'Dispositon from  ED',	'ED SBP',	'ED HR',	'ED RR',	'ED GCS',	'Total Vent Days',	'Total Days in ICU',	'Admission Hosp LOS (days)',	'Total LOS (ED+Admit)',	'Received blood within 4 hrs',	'Severe Brain Injury',	'Time to 1st OR Visit (mins.)',	'Final Outcome-Dead or Alive',	'Discharge Disposition',	'Injury Severity Score',	'AIS 2005',	'Gender_Numerical',	'Burn(Yes/No)',	'Blunt (Yes/No)',	'Penetrating(Yes/No)',	'Report of Physical Abuse(Yes/No)',	'Airbag Deployment (Yes/No)',	'Front Deployment (Yes/No)',	'Side Deployment (Yes/No)',	'Motor_Vehicle_Speed',	'Fall_Height',	'Ground Ambulance (Yes/No)',	'Helicopter Ambulance (Yes/No)',	'Walk-in/Pvt/Public (Yes/No)',	'Resp Assistance (Yes/No)',	'Patient at Front Seat (Yes/No)',	'Patient at Back Seat (Yes/No)',	'Patient at ATV/Quad (3 or more low press. tires/straddleseat/handlebar) (Yes/No)',	'Driver of motor vehicle (not bike)',	'Position - Biclyclist',	'Position - Motorcyclist',	 'Position -Dirt Bike Trail Motorcycle (2 wheel, designed for off-road)',	'Position - Rhino, Side by Side, UTV (steering wheel/non-straddle seats)',	'Positon - Go-Kart',	'Safety Equipment Issues (Yes/No)',	'Arrived from Referring Hospital (Yes/No)',	'Arrived from Scene of Injury',	'Arrived from Home',	'Urgent Care',	'Arrived from Clinic',	'Severe Head Injury - GCS < 9']
+
+X = df[['Gender_Numerical','Burn(Yes/No)','Blunt (Yes/No)','Penetrating(Yes/No)','Report of Physical Abuse(Yes/No)','Airbag Deployment (Yes/No)','Front Deployment (Yes/No)','Side Deployment (Yes/No)','Motor_Vehicle_Speed','Fall_Height','Ground Ambulance (Yes/No)','Helicopter Ambulance (Yes/No)','Walk-in/Pvt/Public (Yes/No)','Resp Assistance (Yes/No)','Patient at Front Seat (Yes/No)','Patient at Back Seat (Yes/No)','Patient at ATV/Quad (3 or more low press. tires/straddleseat/handlebar) (Yes/No)','Severe Head Injury - GCS < 9']]
+
+variable_names = ['Gender_Numerical','Burn(Yes/No)','Blunt (Yes/No)','Penetrating(Yes/No)','Report of Physical Abuse(Yes/No)','Airbag Deployment (Yes/No)','Front Deployment (Yes/No)','Side Deployment (Yes/No)','Motor_Vehicle_Speed','Fall_Height','Ground Ambulance (Yes/No)','Helicopter Ambulance (Yes/No)','Walk-in/Pvt/Public (Yes/No)','Resp Assistance (Yes/No)','Patient at Front Seat (Yes/No)','Patient at Back Seat (Yes/No)','Patient at ATV/Quad (3 or more low press. tires/straddleseat/handlebar) (Yes/No)','Severe Head Injury - GCS < 9']
+
+#print variable_names
+
+#print X.head(2)
+list = []
+
+list = X.values.tolist()
+
+factor = FactorAnalysis().fit(list)
+
+print pd.DataFrame(factor.components_, columns=variable_names)
+
+
+factor.analyze(list, 3, rotation=None)
+
+print factor.loading
+
+factor.get_uniqueness()
+
+factor.get_factor_variance()
+
+
+
