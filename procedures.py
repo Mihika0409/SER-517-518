a59d36db040007b9a807cce568da426b611ca841
pramodkp72 <pramod.kp72@gmail.com>
2018-03-13 20:56:13 -0700

    Task #53 Visualized procedures performed

--- procedures.py
+++ procedures.py
@@ -51,4 +51,94 @@ print len(list_of_levels)
 
 
 
+level1_procedure_dict = {}
+level2_procedure_dict = {}
+
+for i in range(0, len(list_of_procedures_unique)):
+    procedure_level1 = 0;
+    procedure_level2 = 0;
+    for j in range(0, len(list_of_procedures)):
+        if list_of_procedures[j] == list_of_procedures_unique[i] and list_of_levels[j] == '1':
+            procedure_level1 = procedure_level1 + 1
+
+        if list_of_procedures[j] == list_of_procedures_unique[i] and list_of_levels[j] == '2':
+            procedure_level2 = procedure_level2 + 1
+
+    level1_procedure_dict[list_of_procedures_unique[i]] = procedure_level1
+    level2_procedure_dict[list_of_procedures_unique[i]] = procedure_level2
+
+performance = []
+objects = []
+for k, v in level1_procedure_dict.iteritems():
+        print k, v
+        objects.append(k)
+        performance.append(v)
+
+performance_2 = []
+objects_2 = []
+for k, v in level1_procedure_dict.iteritems():
+        print k, v
+        objects_2.append(k)
+        performance_2.append(v)
+
+print "length of object 1:"
+print len(objects)
+print len(performance)
+
+print "length of object 2:"
+print len(objects_2)
+
+
+#Visualizing the data
+# import matplotlib.pyplot as plt;
+#
+# plt.rcdefaults()
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# # objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+# y_pos = np.arange(0, 20 * len(objects), 20)
+# # performance = [10, 8, 6, 4, 2, 1]
+#
+# plt.bar(y_pos, performance, align='edge', alpha=0.5, width = 1.0)
+# plt.xticks(y_pos, objects)
+# plt.ylabel('level 1 cases')
+# plt.title('Count of level 1 for Different procedures:')
+#
+# plt.show()
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# data to plot
+n_groups = len(objects)
+# means_frank = (90, 55, 40, 65)
+# means_guido = (85, 62, 54, 20)
+
+# create plot
+fig, ax = plt.subplots()
+index = np.arange(n_groups)
+bar_width = 0.35
+opacity = 0.8
+
+rects1 = plt.bar(index, performance, bar_width,
+                 alpha=opacity,
+                 color='b',
+                 label='level1')
+
+rects2 = plt.bar(index + bar_width, performance_2, bar_width,
+                 alpha=opacity,
+                 color='g',
+                 label='level2')
+
+plt.xlabel('Procedure')
+plt.ylabel('Count of levels')
+plt.title('Count by procedure')
+plt.xticks(index + bar_width, list_of_procedures_unique)
+plt.legend()
+
+# plt.tight_layout()
+plt.show()
+
 
