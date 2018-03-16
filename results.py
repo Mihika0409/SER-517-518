b152bf895e9e47f142100d6a267ea4cda3907a6b
Chenchu Gowtham Yerrapothu <y.c.gowtham@gmail.com>
2018-03-15 17:27:24 -0700

    Task #47 Compare contrast and results of the existing models

--- results.py
+++ results.py
@@ -45,3 +45,25 @@ print "train Accuracy is ", accuracy_score(y_train,y_pred_train_en)*100
 
 print "testing error is ", (1-accuracy_score(y_test,y_pred_en))*100
 print "training error is ", (1-accuracy_score(y_train,y_pred_train_en))*100
+
+
+def logistic(X,Y):
+    logit_model = sm.Logit(Y, X)
+    result = logit_model.fit()
+    # print(result.summary())
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+    logreg = LogisticRegression(penalty='l1',class_weight='balanced')
+    logreg.fit(X_train, y_train)
+    y_pred = logreg.predict(X_test)
+    print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+    print('Accuracy of logistic regression classifier on training set: {:.2f}'.format(logreg.score(X_train, y_train)))
+    kfold = model_selection.KFold(n_splits=10, random_state=42)
+    modelCV = LogisticRegression(penalty='l1',class_weight='balanced')
+    scoring = 'accuracy'
+    results = model_selection.cross_val_score(modelCV, X_train, y_train, cv=kfold, scoring=scoring)
+    print("10-fold cross validation average accuracy: %.3f" % (results.mean()))
+    print "Model Validation values:"
+    print(classification_report(y_test, y_pred))
+    Confusion_matrix = confusion_matrix(y_test, y_pred)
+    print "Confusion Matrix:"
+    print(Confusion_matrix)
