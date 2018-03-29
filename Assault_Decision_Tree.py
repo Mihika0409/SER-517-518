81f6586aa2103a9d88d45ef024f37e3d6066edf1
pramodkp72 <pramod.kp72@gmail.com>
2018-03-28 18:10:22 -0700

    Task #62 Applied decision tree classifier on the assault data

--- Assault_Decision_Tree.py
+++ Assault_Decision_Tree.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import pandas as pd
+import sklearn.preprocessing as sk
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.tree import DecisionTreeClassifier
 
 df = pd.read_csv('/Users/Pramod/Desktop/SER517/Original datasets/FullData.csv', header = None, error_bad_lines=False)
 df.columns = ['T1#', 'ED/Hosp Arrival Date', 'Age in Years', 'Gender', 'Levels', 'ICD-10 E-code (after 1/2016)', 'ICD-9 E-code (before 2016)',
@@ -45,3 +53,56 @@ print "Number of rows in complete dataframe:"
 print len(result)
 #Printing out the resultant dataframe
 print result
+
+#Removing the null values in all the columns
+list1 = ['T1#', 'ED/Hosp Arrival Date', 'Age in Years', 'Gender', 'Levels', 'ICD-10 E-code (after 1/2016)', 'ICD-9 E-code (before 2016)',
+              'Trauma Type', 'Report of abuse? (started 2014)', 'Injury Comments', 'Airbag Deployment', 'Patient Position in Vehicle',
+              'Safety Equipment Issues', 'Child Restraint', 'MV Speed', 'Fall Height', 'Transport Type', 'Transport Mode', 'Field SBP',
+              'Field HR', 'Field RR', 'Resp Assistance', 'RTS', 'Field GCS', 'Arrived From', 'ED LOS (mins)', 'ED Disposition', 'ED SBP',
+              'ED HR','ED RR', 'ED GCS', 'Total Vent Days', 'Total Days in ICU', 'Admission Hosp LOS (days)', 'Total LOS (ED+Admit)',
+              'Early transfusion? (started 2016)', 'Severe TBI? (started 2016)', 'Time to 1st OR Visit (mins.)', 'Final Outcome-Dead or Alive', 'Hospital Disposition',
+              'Injury Severity Score', 'ICD 9 Dx (before 2016)', 'ICD 10 Dx (after 1/2016)', 'AIS 2005']
+
+for x in list1:
+    result = result[pd.notnull(result[x])]
+
+#Printing the length of the resultant dataframe after removing all the null values
+print len(result)
+# print result
+
+#Only considering the columns we will require
+result = result[['Levels', 'Age in Years', 'Gender','Field SBP', 'Field HR', 'Field RR', 'RTS', 'Field GCS']]
+
+# Taking only the rows with levels 1 and 2 trauma levels
+result = result.loc[result['Levels'].isin(['1', '2'])]
+
+#Replacing Male with 1 and female with 2
+result['Gender'] = result['Gender'].replace(['M', 'F'], value = ['1', '2'])
+
+#Replacing the NA, ND, and Bl rows with a very small value
+list2 = ['Levels', 'Age in Years', 'Gender','Field SBP', 'Field HR', 'Field RR', 'RTS', 'Field GCS']
+for x in list2:
+    result[x] = result[x].replace(['*NA', '*ND', '*BL'], value=['0.0000001', '0.000001', '0.000001'])
+
+print result
+
+#Taking the target variable
+y = result['Levels']
+#print y
+#Taking the input variable
+X = result.drop('Levels', 1)
+print X
+
+# Applying Logistic regression on the pre factors
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+
+classification_pipeline = Pipeline([('pca', PCA(n_components=4)), ('classifier',DecisionTreeClassifier(random_state=1))])
+#
+classification_pipeline.fit(X_train,Y_train)
+
+#compute the accuracy of the model on test data
+accuracy = classification_pipeline.score(X_test, Y_test)
+print('Test Accuracy: %.3f' % accuracy)
+
+accuracy_training = classification_pipeline.score(X_train, Y_train)
+print('Training Accuracy: %.3f' % accuracy_training)
