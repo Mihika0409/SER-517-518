a426dae586610832fed7e5d4eda80f48fae953e7
susmitha8 <skistams@asu.edu>
2018-04-03 22:10:18 -0700

    Task #76 Implementing Random Forest on Assault data

--- randomForestAssault.py
+++ randomForestAssault.py
@@ -10,6 +10,22 @@ from imblearn.over_sampling import SMOTE
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Training the random forest classifier with the scikit learn
+def random_forest_classifier(features, target):
+    clf = RandomForestClassifier()
+    clf.fit(features, target)
+    from sklearn.cross_validation import cross_val_score
+    cross_val = cross_val_score(clf, features, target, cv = 10)
+    return clf, cross_val
+
+def feature_importances(clf, data):
+    feats = {}  # a dict to hold feature_name: feature_importance
+    for feature, importance in zip(data.columns, clf.feature_importances_):
+        feats[feature] = importance  # add the name/value pair
+    importances = pd.DataFrame.from_dict(feats, orient='index').rename(columns={0: 'Gini-importance'})
+    importances.sort_values(by='Gini-importance').plot(kind='bar', rot=45)
+    plt.show()
+
 df = pd.read_csv('/Users/vc/Documents/Susmitha Documents/Software Factory/TraumaDataSixYears.csv', header = None, error_bad_lines=False)
 df.columns = ['T1#', 'ED/Hosp Arrival Date', 'Age in Years', 'Gender', 'Levels', 'ICD-10 E-code (after 1/2016)', 'ICD-9 E-code (before 2016)',
                'Trauma Type', 'Report of abuse? (started 2014)', 'Injury Comments', 'Airbag Deployment', 'Patient Position in Vehicle',
@@ -65,3 +81,20 @@ Y = result['Levels']
 #Input variable
 X = result.drop('Levels', 1)
 print (X)
+
+print(Y.value_counts())
+
+X, Y = make_classification(n_classes=2, class_sep=2,
+                           weights=[0.2, 0.8], n_informative=3, n_redundant=1, flip_y=0,
+                           n_features=8, n_clusters_per_class=1, n_samples=4943, random_state=10)
+print('Original dataset shape {}'.format(Counter(Y)))
+sm = SMOTE(random_state=42)
+#print(df.shape)
+#print(Y.shape)
+
+X_res, Y_res = sm.fit_sample(X, Y)
+#print(Y_res[:10], Y[:10])
+
+clf, cv_scores = random_forest_classifier(X_res, Y_res)
+print(sum(cv_scores)/ len(cv_scores))
+feature_importances(clf, X)
