5a44f1669ca0f7479c8ec717b7229d84d190eca2
Chenchu Gowtham Yerrapothu <y.c.gowtham@gmail.com>
2018-04-03 23:38:20 -0700

    Task #80 DT_fall_intubation_newdata

--- fall_intubation_DecTree.py
+++ fall_intubation_DecTree.py
@@ -25,3 +25,75 @@ where tid <>0 and icd_code like '% fall%'
 or icd_code_9 like '% fall%' 
 and transport_mode <> 'Private/Public Vehicle/Walk-in'
 limit  50000
+
+
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+from sklearn import tree, metrics
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split,cross_val_score
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score
+df = pd.read_csv('/Users/gowtham/Desktop/SER-517&518/Fall_trauma_newdata.csv', header = None, error_bad_lines=False)
+
+df.columns = ['T1',    'ED/Hosp Arrival Date',    'Age in Years',    'Gender',  'Levels',  'ICD-10 E-code','ICD-9 E-code',  'Trauma Type', 'Report of physical abuse', 'Airbag Deployment',   'Patient Position in Vehicle',
+              'Safet Equipment Issues',    'Child Restraint', 'MV Speed',    'Fall Height', 'Transport Type',  'Transport Mode',  'Field SBP',   'Field HR', 'Field RR',    'Resp Assistance',
+              'RTS',   'Field GCS',   'Arrived From',    'ED LOS (mins)',   'Dispositon from  ED', 'ED SBP',  'ED HR',   'ED RR',
+              'ED GCS',    'Total Vent Days', 'Total Days in ICU',   'Admission Hosp LOS (days)',   'Total LOS (ED+Admit)',    'Received blood within 4 hrs', 'Severe Brain Injury', 'Time to 1st OR Visit (mins.)',
+              'Final Outcome-Dead or Alive',   'Discharge Disposition',   'Injury Severity Score', 'AIS 2005']
+
+#print df.head()
+
+df = df[[ 'Age in Years', 'Gender','Fall Height','Field SBP', 'Field HR', 'Field RR', 'RTS', 'Field GCS','Levels']]
+#print df.head()
+#Dropping all the rows with null values
+features = [ 'Age in Years', 'Gender','Fall Height','Field SBP', 'Field HR', 'Field RR', 'RTS', 'Field GCS','Levels']
+for x in features:
+    df = df[pd.notnull(df[x])]
+    df = df[pd.notnull(df[x])].replace(['*NA', '*ND', '*BL'], value=['0', '0', '0'])
+df = df.loc[df['Levels'].isin(['1', '2'])]
+
+le = LabelEncoder()
+df['Gender'] = le.fit_transform(df['Gender'].values)
+
+print df.head()
+
+
+X= df.iloc[:,:-1].values
+y= df['Levels'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+
+dt = tree.DecisionTreeClassifier()
+dt = dt.fit(X_train,y_train)
+
+
+
+# Code to append a new intubated column to data.
+with open('/Users/gowtham/Desktop/SER-517&518/Fall_trauma_newdata.csv', 'rb') as inp, open(
+        '/Users/gowtham/Desktop/SER-517&518/Fall_trauma_newdata1.csv', 'wb') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if "intubated" in row[9].lower():
+            row.append('1')
+        else:
+            row.append('0')
+        writer.writerow(row)
+
+# Modified the RespAssistance Column.
+with open('/Users/gowtham/Desktop/SER-517&518/Fall_trauma_newdata1.csv', 'rb') as inp, open(
+        '/Users/gowtham/Desktop/SER-517&518/Fall_trauma_newdata2.csv', 'wb') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if row[21] == "Assisted Respiratory Rate":
+            row[21] = '1'
+        else:
+            row[21] = '0'
+        writer.writerow(row)
+        
+        
+        
+  
+        
+        
+  
