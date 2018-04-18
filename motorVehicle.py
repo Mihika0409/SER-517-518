88fe3e1a998c99e7a7add32e7049eba044e9563d
Satish Nandan Reddy Lakkam <satish.lakkam@gmail.com>
2018-04-17 20:11:32 -0700

    #89 Implemented Decision Tree Classifier on the modified data.

--- motorVehicle.py
+++ motorVehicle.py
@@ -62,5 +62,26 @@ def logistic(X,Y):
     plt.savefig('Log_ROC')
     plt.show()
     
+# Function to train the Decision Trees Classifier and compute the accuracy on the test set and also cross vaildate using k fold.
+def decisionClassifier(X,Y):
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=100)
+    clf = DecisionTreeClassifier(criterion="gini" , presort=True, random_state=42, class_weight='balanced', splitter='best', max_depth=10, min_samples_leaf=1)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    yTrain_pred = clf.predict(X_train)
+    print "Test Accuracy is ", accuracy_score(y_test, y_pred)
+    print "Training Accuracy is ", accuracy_score(y_train, yTrain_pred)
+    kfold = model_selection.KFold(n_splits=10, random_state=42)
+    modelCV = DecisionTreeClassifier(criterion="gini", presort=True, random_state=42, class_weight='balanced', splitter='best', max_depth=10, min_samples_leaf=1)
+    scoring = 'accuracy'
+    results = model_selection.cross_val_score(modelCV, X_train, y_train, cv=kfold, scoring=scoring)
+    print("10-fold cross validation average accuracy: %.3f" % (results.mean()))
+    Confusion_matrix = confusion_matrix(y_test, y_pred)
+    print "Confusion Matrix:"
+    print(Confusion_matrix)
+    
 print "Efficiency on Logistic Regression Model:"
 logistic(df,Y)
+
+print "Efficiency on Decision Tree Classifier Model:"
+decisionClassifier(df,Y)
